8f3f7fb9cdaea72ecb70ce888c4324702b421ef0
chriskwlee <ckwl.default@gmail.com>
2015-02-16 12:43:03 -0500

    Added SQL files
    Lab 2 SQL script and in-class example

---
+++ ClassExample.sql
@@ -0,0 +1,29 @@
+﻿-- drop table cars;
+create table cars (
+   kind      text,
+   make      text,
+   model     text,
+   priceUSD  numeric
+);
+
+
+insert into cars(priceUSD,   kind,          make,     model)
+         values (98600.42, 'sports coup', 'Porche', '911 Carrera');
+insert into cars(priceUSD,   kind,    make,  model)
+         values (120007.42, 'sedan', 'BMW', '740il');
+insert into cars(priceUSD,   kind,          make,  model)
+         values (121007.42, 'sports coup', 'BMW', 'z7');
+insert into cars(priceUSD, kind,                     make,    model)
+         values (87007.42, 'sports coup/submarine', 'Lotus', 'Esprit');
+
+SELECT make, priceUSD
+FROM cars
+WHERE priceUSD >= 100000
+  or model = 'Esprit'
+
+delete from cars
+where kind = 'sedan'
+
+update cars
+set priceUSD = priceUSD * 5
+where make = 'Lotus'
