bb4c77de21d6514ffd9b32ba4373c947a4d0fc1f
chriskwlee <ckwl.default@gmail.com>
2015-03-07 18:27:45 -0500

    Part of Lab 4
    Intermediate commit.

---
+++ Lab4/Lab4.sql
@@ -0,0 +1,21 @@
+﻿!-- 1. Cities of agents booking an order for a customer whose cid is 'c006'
+
+select city
+from agents
+where aid in 
+	(select aid
+	 from orders
+	 where cid = 'c006')
+
+!-- 2. Pids of products ordered through any agent who takes at least one order from a customer in Kyoto, sorted by pid from highest to lowest
+
+select pid
+from orders
+where aid in
+	(select aid 
+	 from orders
+	 where cid in
+		(select cid
+		 from customers
+		 where city = 'Kyoto'))
+order by pid desc
