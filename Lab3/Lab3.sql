9170860d0ff73f1a3eaf57ed60ba7db30bee6cec
chriskwlee <ckwl.default@gmail.com>
2015-03-07 19:57:49 -0500

    Lab 4
    Rest of Lab 4 completed.

--- Lab3/Lab3.sql
+++ Lab3/Lab3.sql
@@ -1,43 +1,43 @@
-﻿!-- 1. Ordno and dollars of all orders
+﻿-- 1. Ordno and dollars of all orders
 select ordno, dollars
 from orders
 
-!-- 2. Name and city of agents named Smith
+-- 2. Name and city of agents named Smith
 select name, city
 from agents
 where name = 'Smith'
 
-!-- 3. Pid, name, and priceUSD of products with quantity >200,000
+-- 3. Pid, name, and priceUSD of products with quantity >200,000
 select pid, name, priceUSD
 from products
 where quantity > 200000
 
-!-- 4. Names and cities of customers in Dallas
+-- 4. Names and cities of customers in Dallas
 select name, city
 from customers
 where city = 'Dallas'
 
-!-- 5. Names of agents not in New York and not in Tokyo
+-- 5. Names of agents not in New York and not in Tokyo
 select name
 from agents
 where city != 'New York' and city != 'Tokyo'
 
-!-- 6. Data for products not in Dallas or Duluth that cost US$1 or more.
+-- 6. Data for products not in Dallas or Duluth that cost US$1 or more.
 select *
 from products
 where city != 'Dallas' and city != 'Duluth' and priceUSD >= 1
 
-!-- 7. Data for orders in January or May
+-- 7. Data for orders in January or May
 select *
 from orders
 where mon = 'jan' or mon = 'may'
 
-!-- 8. Data for orders in February more than US$500
+-- 8. Data for orders in February more than US$500
 select *
 from orders
 where mon = 'feb' and dollars >= 500
 
-!-- 9. Orders from the customer whose cid is 005
+-- 9. Orders from the customer whose cid is 005
 select *
 from orders
 where cid = 'c005'
--- Lab4/Lab4.sql
+++ Lab4/Lab4.sql
@@ -1,4 +1,4 @@
-﻿!-- 1. Cities of agents booking an order for a customer whose cid is 'c006'
+﻿-- 1. Cities of agents booking an order for a customer whose cid is 'c006'
 
 select city
 from agents
@@ -7,7 +7,7 @@ where aid in
 	 from orders
 	 where cid = 'c006')
 
-!-- 2. Pids of products ordered through any agent who takes at least one order from a customer in Kyoto, sorted by pid from highest to lowest
+-- 2. Pids of products ordered through any agent who takes at least one order from a customer in Kyoto, sorted by pid from highest to lowest
 
 select pid
 from orders
@@ -19,3 +19,51 @@ where aid in
 		 from customers
 		 where city = 'Kyoto'))
 order by pid desc
+
+-- 3. Cids and names of customers who did not place an order through agent a03
+
+select cid, name
+from customers
+where cid in
+	(select cid
+	 from orders
+	 where aid != 'a03')
+
+-- 4. Cids of customers who ordered both product p01 and p07
+
+select cid
+from customers
+where cid in
+	(select cid
+	 from orders
+	 where pid = 'p01' or pid = 'p07')
+
+-- 5. Pids of products not ordered by any customer who placed any order through agent a05
+
+select pid
+from orders
+where cid not in 
+	(select cid
+	 from orders
+	 where aid = 'a05')
+
+-- 6. Name, discounts, and city for all customers who placed orders through agents in Dallas or New York
+
+select name, discount, city
+from customers
+where cid in
+	(select cid
+	 from orders
+	 where aid in
+		(select aid
+		 from agents
+		 where city = 'Dallas' or city = 'New York'))
+
+-- 7. All customers who have the same discount as that of any customers in Dallas or London
+
+select *
+from customers
+where not city = 'Dallas' and not city = 'London' and discount in
+	(select discount 
+	 from customers
+	 where city = 'Dallas' or city = 'London')
