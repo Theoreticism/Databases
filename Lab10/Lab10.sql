786527af04a3830f74a0e91d218b51d820d273c9
chriskwlee <ckwl.default@gmail.com>
2015-04-27 20:48:49 -0400

    Lab 10
    Bit late. Sorry. Git commit but no sync...

---
+++ Lab10/Lab10.sql
@@ -0,0 +1,26 @@
+﻿DROP FUNCTION PreReqsFor(integer);
+CREATE FUNCTION PreReqsFor(integer) RETURNS text as $$
+	SELECT name
+	FROM courses c
+	WHERE c.num in (
+		SELECT prereqnum
+		FROM prerequisites p
+		WHERE p.courseNum = $1
+		)
+$$ LANGUAGE SQL;
+
+DROP FUNCTION IsPreReqFor(integer);
+CREATE FUNCTION IsPreReqFor(integer) RETURNS text as $$
+	SELECT name
+	FROM courses c
+	WHERE c.num in (
+		SELECT coursenum
+		FROM prerequisites p
+		WHERE p.prereqnum = $1
+		)
+$$ LANGUAGE SQL;
+
+SELECT * FROM courses
+SELECT * FROM prerequisites
+SELECT IsPreReqFor(221)
+SELECT PreReqsFor(308)
